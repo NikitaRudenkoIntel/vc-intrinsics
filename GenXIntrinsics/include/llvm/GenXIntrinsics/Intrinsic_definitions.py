a200436070ae25317e15f246891d4ad51c538471
kvladimi <konstantin.vladimirov@intel.com>
2020-04-22 01:30:40 +0300

    Proper closed-source copyrights
    Change-Id: Icfba65bbdea5e8a02a428c1ec1f91ec797cbef96

--- GenXIntrinsics/include/llvm/GenXIntrinsics/Intrinsic_definitions.py
+++ GenXIntrinsics/include/llvm/GenXIntrinsics/Intrinsic_definitions.py
@@ -1,15 +1,27 @@
-#====- Intrinsic_definitions.py - Defines GenX intrinsics ----------------===#
+#===================== begin_copyright_notice ==================================
 #
-#      Copyright (c) 2014 Intel Corporation.
-#      All rights reserved.
+# INTEL CONFIDENTIAL
+# Copyright 2000-2020
+# Intel Corporation All Rights Reserved.
 #
-#        INTEL CORPORATION PROPRIETARY INFORMATION
+# The source code contained or described herein and all documents related to the
+# source code ("Material") are owned by Intel Corporation or its suppliers or
+# licensors. Title to the Material remains with Intel Corporation or its suppliers
+# and licensors. The Material contains trade secrets and proprietary and confidential
+# information of Intel or its suppliers and licensors. The Material is protected by
+# worldwide copyright and trade secret laws and treaty provisions. No part of the
+# Material may be used, copied, reproduced, modified, published, uploaded, posted,
+# transmitted, distributed, or disclosed in any way without Intel's prior express
+# written permission.
 #
-# This software is supplied under the terms of a license
-# agreement or nondisclosure agreement with Intel Corp.
-# and may not be copied or disclosed except in accordance
-# with the terms of that agreement.
+# No license under any patent, copyright, trade secret or other intellectual
+# property right is granted to or conferred upon you by disclosure or delivery
+# of the Materials, either expressly, by implication, inducement, estoppel or
+# otherwise. Any license under such intellectual property rights must be express
+# and approved by Intel in writing.
 #
+#======================= end_copyright_notice ==================================
+
 #===----------------------------------------------------------------------===//
 #
 # This file defines all of the GenX-specific intrinsics, which correspond to
