c9330d3b640111c58dede00f54e5b86a14d20892
Anatoly Parshintsev <anatoly.parshintsev@intel.com>
2020-09-24 23:40:01 +0300

    add missing default initializer to CMSimdCFLower member
    Change-Id: I3ade2d3ec93312737e0e2d19471ac714e55993ee

--- GenXIntrinsics/include/llvm/GenXIntrinsics/GenXSimdCFLowering.h
+++ GenXIntrinsics/include/llvm/GenXIntrinsics/GenXSimdCFLowering.h
@@ -46,7 +46,7 @@ void initializeCMSimdCFLoweringPass(PassRegistry &);
 
 // The worker class for lowering CM SIMD CF
 class CMSimdCFLower {
-  Function *F;
+  Function *F = {};
   // A map giving the basic blocks ending with a simd branch, and the simd
   // width of each one.
   MapVector<BasicBlock *, unsigned> SimdBranches;
